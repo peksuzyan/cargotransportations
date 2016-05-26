3bf0b108e1119f0866a3fc844e21cb9a047de4c0
Pavel Eksuzyan <pavel.eksuzyan@gmail.com>
2016-05-27 02:01:54 +0300

    renamed packages and tested modules interaction

---
+++ servicelayer/src/com/gmail/eksuzyan/pavel/service/core/ServiceManager.java
@@ -0,0 +1,12 @@
+package com.gmail.eksuzyan.pavel.service.core;
+
+import com.gmail.eksuzyan.pavel.dao.core.DaoManager;
+
+/**
+ * Created by expa on 27.05.2016.
+ */
+public class ServiceManager {
+    public static void main(String[] args) {
+        System.out.println(DaoManager.getHelloMessage());
+    }
+}
